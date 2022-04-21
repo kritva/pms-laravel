7ac2de2c0ab49a1dbf02dbe9789d736aa5464bcb
kritva <104065344+kritva@users.noreply.github.com>
2022-04-21 19:52:48 +0530

    home

---
+++ public/home.php
@@ -0,0 +1,25 @@
+@extends('layouts.master_home')
+@section('home_content') 
+<div class="container-fluid"> 
+
+                <!-- start page title -->
+                <div class="page-title-alt-bg"></div>
+                <div class="page-title-box">
+                    <div class="page-title-right">
+                        <ol class="breadcrumb m-0">
+                            <li class="breadcrumb-item"><a href="javascript: void(0);">Dashboard</a></li>
+                        </ol>
+                    </div>
+                    <h4 class="page-title">Dashboard</h4>
+                </div> 
+                <!-- end page title -->
+
+                <div class="row">
+                    
+
+                </div>
+                <!-- end row -->
+                
+            </div> <!-- end container -->
+
+@endsection
